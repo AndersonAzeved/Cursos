68847f7dd5c8e09737c387d324fc0ef16a1b6475
AndersonAzeved <andersonsilva14.2017@gmail.com>
2023-03-14 21:18:06 -0300

    Future

--- aula_08.dart
+++ aula_08.dart
@@ -2,13 +2,23 @@
 
 void main() {
   String nome = "Anderson";
-  String cep = getCepByName("Rua José");
+  //String cep = getCepByName("Rua José");
+  Future<String> cepFuture = getCepByName("Rua José");
+  late String cep;
+
+  cepFuture.then((result) => cep = result);
 
   print(cep);
   print(nome);
 }
 
 // external service
-String getCepByName(String name) {
-  return "59.360-000";
+//String getCepByName(String name) {
+//return "59.360-000";
+//}
+
+// external service
+Future<String> getCepByName(String name) {
+  //simulando requisição
+  return Future.value("59.360-000");
 }
