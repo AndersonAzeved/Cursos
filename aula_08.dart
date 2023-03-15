75ec55829df497660e82a1554574b7af41f6dcbb
AndersonAzeved <andersonsilva14.2017@gmail.com>
2023-03-14 21:20:18 -0300

    Async - Aula Finalizada

--- aula_08.dart
+++ aula_08.dart
@@ -1,21 +1,37 @@
 //Future, Async e Await
 
-void main() {
-  String nome = "Anderson";
-  //String cep = getCepByName("Rua José");
-  Future<String> cepFuture = getCepByName("Rua José");
-  late String cep;
+//Future
 
-  cepFuture.then((result) => cep = result);
+//void main() {
+//String nome = "Anderson";
+//String cep = getCepByName("Rua José");
+//Future<String> cepFuture = getCepByName("Rua José");
+//late String cep;
 
-  print(cep);
-  print(nome);
-}
+//cepFuture.then((result) => cep = result);
+
+// print(cep);
+//print(nome);
+//}
 
 // external service
 //String getCepByName(String name) {
 //return "59.360-000";
 //}
+
+//Async
+void main() async {
+  String nome = "Anderson";
+  //String cep = getCepByName("Rua José");
+  Future<String> cepFuture = getCepByName("Rua José");
+  late String cep;
+
+  //cepFuture.then((result) => cep = result);
+  cep = await cepFuture;
+
+  print(cep);
+  print(nome);
+}
 
 // external service
 Future<String> getCepByName(String name) {
