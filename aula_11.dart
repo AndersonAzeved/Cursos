6132c4c4c57abc54aa1b3bc6f50eabd53191d26c
AndersonAzeved <andersonsilva14.2017@gmail.com>
2023-04-10 13:37:55 -0300

    atualização

--- aula_11.dart
+++
@@ -1,9 +0,0 @@
-//Callabe interface(CALL)
-void main() {
-  BuscarAlunos buscarAlunos = BuscarAlunos();
-  buscarAlunos();
-}
-
-class BuscarAlunos {
-  void call() => print('anderson, azevedo, silva'); //callable interface
-}
